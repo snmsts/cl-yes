2caa9e301b23d38282cdc69e37d04ae8f2f949ab
SANO Masatoshi <snmsts@gmail.com>
2017-11-29 20:46:24 +0900

    bulk write.

--- main.lisp
+++ main.lisp
@@ -1,9 +1,25 @@
 (in-package :cl-user)
 (defpackage :cl-yes
   (:use :cl)
+  (:import-from :cffi)
   (:export :yes))
 (in-package :cl-yes)
 
-(defun yes (out &optional (s "y"))
+(defun prepare (string &optional (length (* 1024 16)) (alignment 4))
+  (setf string (format nil "~A~%" string))
+  (multiple-value-list
+   (cffi:with-foreign-string ((s size) string)
+     (cffi:foreign-string-alloc
+      (if (> (/ length 2) size)
+          (with-output-to-string (o)
+            (loop repeat (* (floor length (* size alignment))
+                            (* size alignment))
+               do (princ string o)))
+          string)))))
+
+(defun yes (&optional (s "y"))
   (loop
-     (write-line s out)))
+     with (buf len) = (prepare s)
+     do #+sbcl (sb-posix:write
+                #.(sb-sys:fd-stream-fd sb-sys:*stdout*) buf len)
+       #-sbcl (error "not supported")))
